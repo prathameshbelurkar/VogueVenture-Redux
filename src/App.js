4b9e78510b2e13d6effae1066ac5b9483bd3b426
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2023-11-25 10:13:30 +0530

    added ruting config, code refactor, added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+import Shop from "./routes/shop/shop.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I'm navbar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,7 @@
+import "./shop.styles.scss";
+
+const Shop = () => {
+  return <h1>I'm Shop</h1>;
+};
+
+export default Shop;
