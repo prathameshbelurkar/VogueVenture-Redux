1e48ace1346fd57a715d3fd1e02dd7f6f5457723
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2023-11-25 10:51:37 +0530

    added nav styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import Shop from "./routes/shop/shop.component";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I'm navbar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 
 const App = () => {
   return (
