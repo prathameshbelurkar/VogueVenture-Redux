320f93c59cd6027a1c939f518779a4e7e87c2a9d
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2023-11-25 15:47:07 +0530

    exported signInWithRedirect from firebase.utils

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -25,6 +25,8 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
