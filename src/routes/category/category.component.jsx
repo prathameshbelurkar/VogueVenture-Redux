a83b3dbfe4a12419bede983ba4cf3c0627f84ed8
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-01-10 17:06:58 +0530

    complete routes/ integration with styled-components

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,9 +2,10 @@ import { Fragment, useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import ProductCard from "../../components/product-card/product-card.component";
-import "./category.styles.scss";
 
 import { CategoriesContext } from "../../contexts/categories.context";
+
+import { CategoryContainer, Title } from "./category.styles.jsx";
 
 const Category = () => {
   const { category } = useParams();
@@ -17,13 +18,13 @@ const Category = () => {
 
   return (
     <Fragment>
-      <h2 className="category-title">{category.toUpperCase()}</h2>
-      <div className="category-container">
+      <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
+      </CategoryContainer>
     </Fragment>
   );
 };
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,39 +1,49 @@
 import { useContext } from "react";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
-import "./checkout.styles.scss";
 
 import { CartContext } from "../../contexts/cart.context";
+
+import {
+  CheckoutContainer,
+  CheckoutHeader,
+  HeaderBlock,
+  Total,
+} from "./checkout.styles.jsx";
 
 const Checkout = () => {
   const { cartItems, cartTotal } = useContext(CartContext);
 
   return (
-    <div className="checkout-container">
-      <div className="checkout-header">
-        <div className="header-block">
+    <CheckoutContainer>
+      <CheckoutHeader>
+        <HeaderBlock>
           <span>Description</span>
-        </div>
-        <div className="header-block">
+        </HeaderBlock>
+
+        <HeaderBlock>
           <span>Peoduct</span>
-        </div>
-        <div className="header-block">
+        </HeaderBlock>
+
+        <HeaderBlock>
           <span>Quantity</span>
-        </div>
-        <div className="header-block">
+        </HeaderBlock>
+
+        <HeaderBlock>
           <span>Price</span>
-        </div>
-        <div className="header-block">
+        </HeaderBlock>
+
+        <HeaderBlock>
           <span>Remove</span>
-        </div>
-      </div>
+        </HeaderBlock>
+      </CheckoutHeader>
 
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
 
-      <span className="total">Total: ${cartTotal !== 0 ? cartTotal : 0}</span>
-    </div>
+      <Total>Total: ${cartTotal !== 0 ? cartTotal : 0}</Total>
+    </CheckoutContainer>
   );
 };
 
